be4af2b37b9d6d009131f0641a849c30336baff8
jo2ozw <82762241+jo2ozw@users.noreply.github.com>
2021-04-29 21:22:18 +0900

    initialcommit
    プラクティス

---
+++ Reception_desk.py
@@ -0,0 +1,25 @@
+﻿def Reception_desk(name):
+ import streamlit as st
+# import speaktest as sp
+
+ choice = st.sidebar.selectbox("メニュー",('menu','regist','training','buy','sell'))
+ 
+ if choice == "regist":
+ 	st.subheader("this is Adventurer registration")
+ 
+ elif choice == "LOGIN":
+ 		st.subheader("this is login screen")
+ elif choice == "training":
+ 		st.subheader("this is training")
+ 
+ 
+ import pandas as pd
+ import numpy as np
+ import pandas as pd
+# a = sp.speak()
+ st.subheader("初めてギルドにいらした方は ")
+ st.subheader("最初に登録をお済ませください。")
+ st.subheader("登録がお済みになられてから ")
+ st.subheader("ギルドのサービスをご利用できます。 ")
+ h_name = st.text_input ("ハンドルネームを下のテキストボックスにご入力ください。")
+ return
---
+++ pd2.py
@@ -0,0 +1,19 @@
+﻿#def Reception_(name)
+import pandas as pd
+import numpy as np
+import pandas as pd
+import regist as rg
+import streamlit as st
+#import speaktest as sp
+st.header('受付窓口 チェック')
+name = st.text_input('what your name')
+choice = st.sidebar.selectbox("メニュー",('menu','冒険者登録','training','buy','sell'))
+
+#if choice == "regist":
+st.subheader("this is Adventurer registration")
+rg.Registration(name)
+#elif choice == "buy":
+st.subheader("this is login screen")
+#elif choice == "training":
+st.subheader("this is training")
+
